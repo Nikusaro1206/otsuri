8cf4238fe8f85cadd030858b3fe8bcffaa625950
Nikusaro1206 <syrugs.hiroaki@gmail.com>
2024-10-18 19:02:23 +0900

    関数実装

--- コイン判定.py
+++ コイン判定.py
@@ -1,38 +1,30 @@
+def maisuu_keisan(i,nyuukin):
+    otkr = nyuukin // coin[i]
+    print(otkr)
+    if maisuu[i] >= otkr:
+        ot[i] = otkr
+        return nyuukin % coin[i]
+    else:
+        ot[i] = maisuu[i]
+        return nyuukin - (coin[i] * ot[i])
+
 nyuukin = int(input("いくら入れましたか？"))
 ryoukin = int(input("いくらのものを買いますか"))
 
-nyuukin = nyuukin - ryoukin
 maisuu = [1 , 10 , 10 , 10]
-if maisuu[0] > 0:
-    otsuri_500 = nyuukin // 500
-    nyuukin = nyuukin % 500
-else:
-    otsuri_500 = 0
+coin = [500 , 100 , 50 ,10]
+ot = [0 , 0 , 0, 0]
 
-otsuri_100_kari = nyuukin // 100 
-if maisuu[1] > otsuri_100_kari:
-    otsuri_100 = nyuukin // 100
-    nyuukin = nyuukin % 100
-else:
-    otsuri_100 = maisuu[1]
-    nyuukin = nyuukin - 100 * maisuu[1]
+nyuukin = nyuukin - ryoukin
 
-otsuri_50_kari = nyuukin // 50 
-if maisuu[2] > otsuri_50_kari:
-    otsuri_50 = nyuukin // 50
-    nyuukin = nyuukin % 50
-else:
-    otsuri_50 = maisuu[2]
-    nyuukin = nyuukin - 50 * maisuu[2]
+for i in range (0,4):
+    print(ot)
+    nyuukin = maisuu_keisan(i,nyuukin)
 
-otsuri_10_kari = nyuukin // 10
-if maisuu[3] > otsuri_10_kari:
-    otsuri_10 = nyuukin // 10
-    nyuukin = nyuukin % 10
-
-    print(f"500円玉:{otsuri_500}")
-    print(f"100円玉:{otsuri_100}")
-    print(f"50円玉:{otsuri_50}")
-    print(f"10円玉:{otsuri_10}")
+if nyuukin == 0:
+    print(f"500円玉:{ot[0]}")
+    print(f"100円玉:{ot[1]}")
+    print(f"50円玉:{ot[2]}")
+    print(f"10円玉:{ot[3]}")
 else:
     print("精算できません")
