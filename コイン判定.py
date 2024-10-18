144114f5dacfdab62fdfbc151a3d3b89d33487c4
Nikusaro1206 <syrugs.hiroaki@gmail.com>
2024-10-18 19:18:08 +0900

    変数名を少し変更

--- コイン判定.py
+++ コイン判定.py
@@ -1,30 +1,28 @@
 def maisuu_keisan(i,nyuukin):
-    otkr = nyuukin // coin[i]
-    print(otkr)
-    if maisuu[i] >= otkr:
-        ot[i] = otkr
+    otr_kari = nyuukin // coin[i]
+    if maisuu[i] >= otr_kari:
+        otr[i] = otr_kari
         return nyuukin % coin[i]
     else:
-        ot[i] = maisuu[i]
-        return nyuukin - (coin[i] * ot[i])
-
-nyuukin = int(input("いくら入れましたか？"))
+        otr[i] = maisuu[i]
+        return nyuukin - (coin[i] * otr[i])
+    
 ryoukin = int(input("いくらのものを買いますか"))
+nyuukin = int(input("いくら入れましたか？"))
 
 maisuu = [1 , 10 , 10 , 10]
 coin = [500 , 100 , 50 ,10]
-ot = [0 , 0 , 0, 0]
+otr = [0 , 0 , 0, 0]
 
 nyuukin = nyuukin - ryoukin
 
 for i in range (0,4):
-    print(ot)
     nyuukin = maisuu_keisan(i,nyuukin)
 
 if nyuukin == 0:
-    print(f"500円玉:{ot[0]}")
-    print(f"100円玉:{ot[1]}")
-    print(f"50円玉:{ot[2]}")
-    print(f"10円玉:{ot[3]}")
+    print(f"500円玉:{otr[0]}")
+    print(f"100円玉:{otr[1]}")
+    print(f"50円玉:{otr[2]}")
+    print(f"10円玉:{otr[3]}")
 else:
     print("精算できません")
