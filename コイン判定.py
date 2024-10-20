1e8c942034b7bca40afa8eadf7293d61da3dabf3
Nikusaro1206 <syrugs.hiroaki@gmail.com>
2024-10-20 16:56:46 +0900

    釣り銭残量をGUIから変えられる機能を実装

--- コイン判定.py
+++ コイン判定.py
@@ -63,25 +63,26 @@ class Aplication(tk.Frame):
         turi_in500_sp = tk.Label(turi_in_sp,text="500円玉:",width=7)
         self.turi_in500_Text = tk.StringVar()
         self.turi_in500_Text.set(maisuu[0])
-        turi_in500_label = tk.Label(turi_in_sp,textvariable=self.turi_in500_Text)
+        self.turi_in500_label = tk.Entry(turi_in_sp,textvariable=self.turi_in500_Text,width=4)
         turi_in100_sp = tk.Label(turi_in_sp,text="100円玉:",width=7)
         self.turi_in100_Text = tk.StringVar()
         self.turi_in100_Text.set(maisuu[1])
-        turi_in100_label = tk.Label(turi_in_sp,textvariable=self.turi_in100_Text)
+        self.turi_in100_label = tk.Entry(turi_in_sp,textvariable=self.turi_in100_Text,width=4)
         turi_in50_sp = tk.Label(turi_in_sp,text="50円玉:",width=7)
         self.turi_in50_Text = tk.StringVar()
         self.turi_in50_Text.set(maisuu[2])
-        turi_in50_label = tk.Label(turi_in_sp,textvariable=self.turi_in50_Text)
+        self.turi_in50_label = tk.Entry(turi_in_sp,textvariable=self.turi_in50_Text,width=4)
         turi_in10_sp = tk.Label(turi_in_sp,text="10円玉:",width=7)
         self.turi_in10_Text = tk.StringVar()
         self.turi_in10_Text.set(maisuu[3])
-        turi_in10_label = tk.Label(turi_in_sp,textvariable=self.turi_in10_Text)
+        self.turi_in10_label = tk.Entry(turi_in_sp,textvariable=self.turi_in10_Text,width=4)
+        turi_rescan_btn = tk.Button(turi_in_sp,text="更新",width=3,command = self.turi_in_entry_scan)
 
         #ウィジェット配置
         line1 = 45
         line2 = 45
         line3 = 150
-        line4 = 250
+        line4 = 230
         line5 = 250
         self.input_coin.place(relx=0.5,y=line1,anchor=tk.CENTER)
         coin_btn_1000.grid(in_ =self.input_coin,row=0,column=0)
@@ -117,13 +118,14 @@ class Aplication(tk.Frame):
 
         turi_in_sp.place(relx=0.1,y=line4)
         turi_in500_sp.grid(in_ = turi_in_sp,row=0,column = 0)
-        turi_in500_label.grid(in_ = turi_in_sp,row=0,column = 1)
+        self.turi_in500_label.grid(in_ = turi_in_sp,row=0,column = 1)
         turi_in100_sp.grid(in_ = turi_in_sp,row=1,column = 0)
-        turi_in100_label.grid(in_ = turi_in_sp,row=1,column = 1)
+        self.turi_in100_label.grid(in_ = turi_in_sp,row=1,column = 1)
         turi_in50_sp.grid(in_ = turi_in_sp,row=2,column = 0)
-        turi_in50_label.grid(in_ = turi_in_sp,row=2,column = 1)
+        self.turi_in50_label.grid(in_ = turi_in_sp,row=2,column = 1)
         turi_in10_sp.grid(in_ = turi_in_sp,row=3,column = 0)
-        turi_in10_label.grid(in_ = turi_in_sp,row=3,column = 1)
+        self.turi_in10_label.grid(in_ = turi_in_sp,row=3,column = 1)
+        turi_rescan_btn.grid(in_ = turi_in_sp,row = 4,column=1)
         #GUI部分終了
 
     def sentaku_click(self,i):#処理部分
@@ -144,13 +146,13 @@ class Aplication(tk.Frame):
     def total_coin(self,kingaku):
         in_coin[kingaku] = in_coin[kingaku] + 1
         self.nyuukin = self.nyuukin + coin[kingaku]
-        print(self.nyuukin)
+        #print(self.nyuukin)
         self.total_Text.set(self.nyuukin)
-        print(in_coin)
+        #print(in_coin)
 
     def maisuu_keisan(self,i,nyuukin):
         otr_kari = nyuukin // coin[i]
-        print(otr_kari)
+        #print(otr_kari)
         if maisuu[i] >= otr_kari:
             otr[i] = otr_kari
             return nyuukin % coin[i]
@@ -162,18 +164,18 @@ class Aplication(tk.Frame):
 
         if nyuukin == 0:
             
-            print(f"500円玉:{otr[0]}")
-            print(f"100円玉:{otr[1]}")
-            print(f"50円玉:{otr[2]}")
-            print(f"10円玉:{otr[3]}")
+            #print(f"500円玉:{otr[0]}")
+            #print(f"100円玉:{otr[1]}")
+            #print(f"50円玉:{otr[2]}")
+            #print(f"10円玉:{otr[3]}")
             for i in range (0,4):
                 maisuu[i] = maisuu[i] - otr[i]
                 maisuu[i] = maisuu[i] + in_coin[i]
             self.turi_rescan()
         else:
             tkmg.showinfo("精算できません","釣り銭切れもしくは金額不足")
-            print("精算できません")
-        print(maisuu)
+            #print("精算できません")
+        #print(maisuu)
         self.stats_reset()
 
     def stats_reset(self):
@@ -202,6 +204,14 @@ class Aplication(tk.Frame):
         self.turi_in100_Text.set(maisuu[1])
         self.turi_in50_Text.set(maisuu[2])
         self.turi_in10_Text.set(maisuu[3])
+
+    def turi_in_entry_scan(self):
+        maisuu[0] = int(self.turi_in500_label.get())
+        maisuu[1] = int(self.turi_in100_label.get())
+        maisuu[2] = int(self.turi_in50_label.get())
+        maisuu[3] = int(self.turi_in10_label.get())
+        self.turi_in_rescan()
+        #print(maisuu)
     #処理部分終了
 
 
